4dbcbbf119fcd4f98517a9834d1f0edcee5c6801
Ahmed Heikal <aheikal424@icloud.com>
2023-12-05 01:30:03 +0200

    create ask a question page

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -1,7 +1,15 @@
 import React from "react";
+import Question from "@/components/forms/Question";
 
 const AskQuestion = () => {
-  return <section className="">AskQuestion</section>;
+  return (
+    <section className="">
+      <h1 className="h1-bold text-dark100_light900">Ask a public quesiton</h1>
+      <div className="mt-9">
+        <Question />
+      </div>
+    </section>
+  );
 };
 
 export default AskQuestion;
