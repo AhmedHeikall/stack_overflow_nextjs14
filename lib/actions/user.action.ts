12c3d0c9184ea1c13ed8cf035ff774ebf57ffeb6
Ahmed Heikal <aheikal424@icloud.com>
2024-01-04 08:29:04 +0200

    comment

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -65,9 +65,9 @@ export async function deleteUser(userData: DeleteUserParams) {
     // Delete user from database and their questions, comments etc..
 
     // get user Questions ids
-    const userQuestionsIds = await Question.find({ author: user._id }).distinct(
-      "_id"
-    );
+    // const userQuestionsIds = await Question.find({ author: user._id }).distinct(
+    //   "_id"
+    // );
 
     // delete user questions
     await Question.deleteMany({ author: user._id });
