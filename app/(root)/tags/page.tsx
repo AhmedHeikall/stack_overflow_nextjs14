df93a07810c0a28ae1276eb29334feccf0a4fa98
Ahmed Heikal <aheikal424@icloud.com>
2024-01-24 00:15:35 +0200

    create Tags page

---
+++ app/(root)/tags/page.tsx
@@ -0,0 +1,69 @@
+import React from "react";
+import Link from "next/link";
+import Filter from "@/components/shared/filter/Filter";
+import LocalSearchbar from "@/components/shared/search/LocalSearchbar";
+import NoResult from "@/components/shared/noresult/NoResult";
+
+import { TagFilters } from "@/constants/filters";
+import { getAllTags } from "@/lib/actions/tag.action";
+
+const Tags = async () => {
+  const result = await getAllTags({});
+
+  return (
+    <>
+      <div className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center">
+        <h1 className="h1-bold text-dark100_light900">Tags</h1>
+      </div>
+      <div className="mt-11 flex justify-between gap-5 max-sm:flex-col sm:items-center">
+        <LocalSearchbar
+          route="/community"
+          iconPosition="left"
+          imgSrc="/assets/icons/search.svg"
+          placeholder="Search for best minds here..."
+          otherClasses="flex-1"
+        />
+        <Filter
+          filters={TagFilters}
+          otherClasses="min-h-[56px] sm:min-w-[170px]"
+        />
+      </div>
+
+      <div className="mt-12 flex flex-wrap  gap-4">
+        {/* Looping throw devs cards */}
+        {result.tags.length > 0 ? (
+          result.tags.map((tag) => (
+            <Link
+              href={`/tags/${tag._id}`}
+              key={tag._id}
+              className="w-full  max-xs:min-w-full xs:w-[260px] "
+            >
+              <article className="background-light900_dark200 light-border flex w-full flex-col  rounded-2xl border p-8 py-10 shadow-md">
+                <div className="background-light800_dark400 w-fit rounded-sm px-5 py-1.5">
+                  <p className="paragraph-semibold text-dark300_light900">
+                    {tag.name}
+                  </p>
+                </div>
+                <p className="small-medium text-dark400_light500 mt-3.5 ">
+                  <span className="body-semibold primary-text-gradient mr-2.5">
+                    {tag.questions.length}+
+                  </span>{" "}
+                  Questions
+                </p>
+              </article>
+            </Link>
+          ))
+        ) : (
+          <NoResult
+            title="There’s no Tags to show"
+            descrption="It looks like there are no tags found."
+            link="/ask-question"
+            linkTitle="Ask a question"
+          />
+        )}
+      </div>
+    </>
+  );
+};
+
+export default Tags;
--- lib/actions/tag.action.ts
+++ lib/actions/tag.action.ts
@@ -1,8 +1,10 @@
 "use server";
 
 import { connectToDatabase } from "../mongoose";
-import { GetTopInteractedTagsParams } from "./shared.types";
+import { GetTopInteractedTagsParams, GetAllTagsParams } from "./shared.types";
+
 import User from "@/database/user.model";
+import Tag from "@/database/tag.model";
 
 export async function getTopInteractedTags(params: GetTopInteractedTagsParams) {
   try {
@@ -22,6 +24,19 @@ export async function getTopInteractedTags(params: GetTopInteractedTagsParams) {
       { _id: "2", name: "tag2" },
       { _id: "3", name: "tag3" },
     ];
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+
+export async function getAllTags(params: GetAllTagsParams) {
+  try {
+    connectToDatabase();
+
+    const tags = await Tag.find({}).sort({ createdAt: -1 });
+
+    return { tags };
   } catch (error) {
     console.log(error);
     throw error;
